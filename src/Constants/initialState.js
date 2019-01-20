4329265876ea7cf10e1094f2e29d7ae82d03cdc9
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-20 01:45:24 +0000

    working redux model

---
+++ src/Constants/initialState.js
@@ -0,0 +1,38 @@
+export const initialState = {
+  todos: [
+    {
+      title: '1st Random Item',
+      id: 0,
+      isComplete: false,
+    },
+    {
+      title: '2nd Random Item',
+      id: 1,
+      isComplete: false,
+    },
+    {
+      title: '1st Random Item',
+      id: 3,
+      isComplete: false,
+    },
+    {
+      title: '2nd Random Item',
+      id: 4,
+      isComplete: false,
+    },
+    {
+      title: '1st Random Item',
+      id: 5,
+      isComplete: false,
+    },
+    {
+      title: '2nd Random Item',
+      id: 6,
+      isComplete: false,
+    }, {
+      title: '2nd Random Item',
+      id: 7,
+      isComplete: false,
+    },
+  ],
+}
